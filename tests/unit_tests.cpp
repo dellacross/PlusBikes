95e8fa141c5207cfa457488ec5c5cf66cbf01847
dellacross <victordella652@gmail.com>
2023-12-01 14:57:20 -0300

    Add Tests

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,12 +1,19 @@
 #include "map.hpp"
 #include "matrixCells.hpp"
+#include <sstream>
+#include <fstream>
 #include "CppUTest/TestHarness.h"
+
+vector<int> getDimensions(fstream &file);
+bool BikeOrVisitor(char s);
+int getElementID(char character);
+bool ifExists(int x, vector<int> q);
 
 TEST_GROUP(MapTests) {
     Map* map;
 
     void setup() {
-        map = new Map(10, 10, 12);
+        map = new Map(12, 10, 10);
         map->initMapMatrix();
     }
 
@@ -14,8 +21,41 @@ TEST_GROUP(MapTests) {
         delete map;
     }
 };
-
 // *1*
+TEST(MapTests, getDimensions) {
+    string line = "";
+    fstream file("/tests/teste1.txt");
+    vector<int> values = getDimensions(file);
+
+    int _numberOfElements = values[0], _dimX = values[1], _dimY = values[2];
+
+    CHECK_EQUAL(_numberOfElements, 12);
+    CHECK_EQUAL(_dimX, 10);
+    CHECK_EQUAL(_dimY, 10);
+}
+
+// *2*
+TEST(MapTests, ifExists) {
+    vector<int> _vector;
+    _vector.push_back(1);
+    _vector.push_back(2);
+    _vector.push_back(3);
+
+    CHECK_TRUE(ifExists(1, _vector));
+    CHECK_TRUE(ifExists(2, _vector));
+    CHECK_TRUE(ifExists(3, _vector));
+}
+
+// *3*
+TEST(MapTests, getElementID) {
+    CHECK_EQUAL(getElementID('0'), 1);
+    CHECK_EQUAL(getElementID('5'), 6);
+    CHECK_EQUAL(getElementID('9'), 10);
+    CHECK_EQUAL(getElementID('a'), 1);
+    CHECK_EQUAL(getElementID('d'), 5);
+}
+
+// *4*
 TEST(MapTests, InitMapMatrix) {
     int _dimX = map->getDimX();
     int _dimY = map->getDimY();
@@ -26,7 +66,7 @@ TEST(MapTests, InitMapMatrix) {
     CHECK_EQUAL(_numElements, 12);
 }
 
-// *2* 
+// *5* 
 TEST(MapTests, cell) {
     Cell *cell;
     cell = new Cell(-1, 2, false, false);
@@ -36,7 +76,7 @@ TEST(MapTests, cell) {
     CHECK_EQUAL(2, cell->getBikeID());
 }
 
-// *3*
+// *6*
 TEST(MapTests, UpdateMapMatrixCell) {
     map->updateMapMatrixCell(2, 3, true, -1, -1);
     map->updateMapMatrixCell(1, 4, false, 2, -1);
@@ -47,7 +87,7 @@ TEST(MapTests, UpdateMapMatrixCell) {
     CHECK_EQUAL(2, _map[1][4].getBikeID());
 }
 
-// *4*
+// *7*
 TEST(MapTests, checkCell) {
     bool invalid_out_of_range_1 = map->checkCell(11, 3);
     bool invalid_out_of_range_2 = map->checkCell(1, 13);
@@ -60,7 +100,7 @@ TEST(MapTests, checkCell) {
     CHECK_FALSE(invalid_out_of_range_4);
 }
 
-// *5*
+// *8*
 TEST(MapTests, checkCell) {
     bool invalid_negative_1 = map->checkCell(-1, 3);
     bool invalid_negative_2 = map->checkCell(1, -3);
@@ -69,10 +109,7 @@ TEST(MapTests, checkCell) {
     CHECK_FALSE(invalid_negative_2);
 }
 
-// *6*
-
-
-// *7*
+// *9*
 TEST(MapTests, initVisitorsPreferenceMatrix) {
     map->initVisitorsPreferenceMatrix();
     pair<int, int>** _visitorMatrix = map->getVisitorsPreferenceMatrix();
@@ -81,7 +118,7 @@ TEST(MapTests, initVisitorsPreferenceMatrix) {
     CHECK_FALSE(_nullptr);
 }
 
-// *8*
+// *10*
 TEST(MapTests, initBikesPreferenceMatrix) {
     map->initBikesPreferenceMatrix();
     pair<int, int>** _bikesMatrix = map->getBikesPreferenceMatrix();
@@ -90,7 +127,7 @@ TEST(MapTests, initBikesPreferenceMatrix) {
     CHECK_FALSE(_nullptr);
 }
 
-// *9*
+// *11*
 TEST(MapTests, initCoordOfBikesVector) {
     map->initCoordOfBikesVector();
     pair<int, int>* _coordsOfBikes = map->getCoordsOfBikes();
@@ -99,7 +136,7 @@ TEST(MapTests, initCoordOfBikesVector) {
     CHECK_FALSE(_nullptr);
 }
 
-// *10*
+// *12*
 TEST(MapTests, initCoordOfVisitorsVector) {
     map->initCoordOfVisitorsVector();
     pair<int, int>* _coordsOfVisitors = map->getCoordsOfVisitors();
@@ -107,11 +144,6 @@ TEST(MapTests, initCoordOfVisitorsVector) {
     if(_coordsOfVisitors == nullptr) _nullptr = true;
     CHECK_FALSE(_nullptr);
 }
-
-// *11*
-
-// *12*
-
 
 // *13*
 
