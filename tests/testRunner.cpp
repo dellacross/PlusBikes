adc0f8b6cf8e18356f9151e0c31fc5d8e9019d84
dellacross <victordella652@gmail.com>
2023-12-03 15:24:15 -0300

    Searching error

--- tests/testRunner.cpp
+++ tests/testRunner.cpp
@@ -405,7 +405,8 @@ int main(int argc, char** argv) {
     cout << "Success! All unit tests passed!" << "\n";
     
     cout << "Starting system tests..." << "\n";
-
+    
+    cout << "entrando no exec_system" << "\n";
     exec_system(argv[1]);
     cout << "passou do exec_system" << "\n";
     //compareFiles(argv[1]);
